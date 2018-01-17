4d84f0f7ceb057915b4e21efa036211611e5ee37
tanner <tanner.drake@protonmail.com>
2018-01-17 16:28:50 -0500

    add button onclick and square state

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+
+    render() {
+        return (
+        <button className="square" onClick={() => {alert('clickity click');}}>
+            {this.props.value}
+        </button>
+        );
+    }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
